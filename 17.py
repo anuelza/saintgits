0034939699996bab259524407105924bc179c237
anuelza <96429933+anuelza@users.noreply.github.com>
2022-03-15 10:28:24 +0530

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,6 @@
+x=int(input("Enter the first integer:"))
+y=int(input("Enter the second integer:"))
+if(x>y):
+    print("The first ineteger",x,"is greater than the second integer",y ,".")
+else:
+    print("The second integer ",y,"is greater than the first integer",x ,".")
---
+++ 20.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the number:"))
+if(a>0):
+    print("The given number is positive.")
+elif(a==0):
+    print("The given number is 0.")
+else:
+    print("The given number is negative.")
